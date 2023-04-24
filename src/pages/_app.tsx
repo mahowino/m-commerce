0537097634d6b92f10db7fa536c2fe0307fb6e6e
poolr-ridesharing <84660656+poolr-ridesharing@users.noreply.github.com>
2023-04-24 22:19:26 +0300

    login trial

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,23 @@
 import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
 import '@/styles/globals.css'
+import { NextPage } from 'next'
 import type { AppProps } from 'next/app'
+import { ReactElement, ReactNode } from 'react'
 
-export default function App({ Component, pageProps }: AppProps) {
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
 
-  return(
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+//to fix this
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((login) => login)
+  return getLayout(
   <>
   <Navbar />
   <Component {...pageProps} />
